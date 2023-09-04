ac299a684b2e3855eea12e1e48ff69c01e6f8261
rzafft1 <rzafft1@uwyo.edu>
2023-09-04 12:09:42 -0600

    fixed code.js, added some comments

--- code.js
+++ code.js
@@ -1,5 +1,11 @@
+// simple change should fix this
+// original code had 'var sum = a[0]'...
+// this indicates that the variable sum wil be initialized to the value of the 
+// ... first element in the array/list. This cant be right. 
+// ... lets change the var sum to be initialized to 0. This should fix it
+
 function sum(a) {
-    var sum = a[0];
+    var sum = 0;
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
